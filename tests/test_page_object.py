50ef56b23954f173cda2cb529b447bcb041ab5be
Maciej Fec <maciejfec1996@gmail.com>
2023-03-30 21:10:55 +0100

    add more tests

--- tests/test_page_object.py
+++ tests/test_page_object.py
@@ -11,3 +11,53 @@ def test_open():
     assert current_url == url_to_open
 
     login_page.quit()
+
+def test_click_on():
+    username = "standard_user"
+    password = "secret_sauce"
+    url_to_open = "https://www.saucedemo.com/"
+    login_page = SwagLabsLoginPage()
+    login_page.open(url_to_open)
+    login_page.login(username, password)
+
+    assert login_page.get_current_url() != url_to_open
+
+    login_page.quit()
+
+def test_type_into():
+    username = "test_user"
+    url_to_open = "https://www.saucedemo.com/"
+    login_page = SwagLabsLoginPage()
+    login_page.open(url_to_open)
+    login_page.input_username(username)
+    username_field_value = login_page.get_username_field_value()
+
+    assert username_field_value == username
+
+    login_page.quit()
+
+def test_clear():
+    username = "test_user"
+    url_to_open = "https://www.saucedemo.com/"
+    login_page = SwagLabsLoginPage()
+    login_page.open(url_to_open)
+    login_page.input_username(username)
+    login_page.clear_username_field()
+    username_field_value = login_page.get_username_field_value()
+
+    assert username_field_value == ""
+
+    login_page.quit()
+
+def test_get_text():
+    standard_username = "standard_user"
+    url_to_open = "https://www.saucedemo.com/"
+    login_page = SwagLabsLoginPage()
+    login_page.open(url_to_open)
+    
+    credentials_text = login_page.get_login_credentials_text()
+
+    assert standard_username in credentials_text
+
+    login_page.quit()
+
